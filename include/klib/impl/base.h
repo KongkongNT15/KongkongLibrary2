ae22549fbedf935a9d29cdd7459a674ded5a0206
KongkongNT15 <kongkong745837@gmail.com>
2025-08-01 14:09:27 +0900

    記念すべき最初のコミット

---
+++ include/klib/impl/base.h
@@ -0,0 +1,231 @@
+﻿#ifndef KLIB_BASE_H
+#define KLIB_BASE_H
+
+#include <stdint.h>
+#include <concepts>
+#include <type_traits>
+
+#if __has_include(<Windows.h>)
+    #define KLIB_ENV_WINDOWS 1
+
+    #if __cplusplus == 199711L
+        #define KLIB_COMPILER_MSVC 1
+        #define KLIB_CPP_LANG_VERSION _MSVC_LANG
+
+        #if defined(_M_ARM64)
+            #define KLIB_ENV_ARM64 1
+        #elif defined(_M_X64)
+            #define KLIB_ENV_X64 1
+        #endif
+    #else
+        #define KLIB_COMPILER_MSVC 0
+        #define KLIB_CPP_LANG_VERSION __cplusplus
+    #endif
+
+    #if defined(__MINGW32__) || defined(__MINGW64__)
+        #define KLIB_COMPILER_MINGW 1
+        #if defined(__aarch64__)
+            #define KLIB_ENV_ARM64 1
+        #elif defined(__x86_64__)
+            #define KLIB_ENV_X64 1
+        #endif
+    #endif
+#endif
+
+#if __has_include(<unistd.h>) && !defined(KLIB_ENV_WINDOWS)
+    #define KLIB_ENV_UNIX 1
+#endif
+
+#ifdef __APPLE__
+    #define KLIB_ENV_APPLE 1
+
+    #if defined(__aarch64__) || defined(__arm64__)
+        #define KLIB_ENV_ARM64 1
+    #else
+        #define KLIB_ENV_X64 1
+    #endif
+#endif
+
+#ifdef __linux__
+    #define KLIB_ENV_LINUX 1
+
+    #ifdef __aarch64__
+        #define KLIB_ENV_ARM64 1
+    #endif
+
+    #ifdef __x86_64__
+        #define KLIB_ENV_X64 1
+    #endif
+#endif
+
+#ifndef KLIB_CPP_LANG_VERSION
+    #define KLIB_CPP_LANG_VERSION __cplusplus
+#endif
+
+#define KLIB_CPP11 201103L
+#define KLIB_CPP14 201402L
+#define KLIB_CPP17 201703L
+#define KLIB_CPP20 202002L
+#define KLIB_CPP23 202302L
+
+#if KLIB_CPP_LANG_VERSION >= KLIB_CPP11
+    #define KLIB_HAS_CPP11 1
+#else
+    #define KLIB_HAS_CPP11 0
+#endif
+
+#if KLIB_CPP_LANG_VERSION >= KLIB_CPP14
+    #define KLIB_HAS_CPP14 1
+#else
+    #define KLIB_HAS_CPP14 0
+#endif
+
+#if KLIB_CPP_LANG_VERSION >= KLIB_CPP17
+    #define KLIB_HAS_CPP17 1
+#else
+    #define KLIB_HAS_CPP17 0
+#endif
+
+#if KLIB_CPP_LANG_VERSION >= KLIB_CPP20
+    #define KLIB_HAS_CPP20 1
+#else
+    #define KLIB_HAS_CPP20 0
+#endif
+
+#if KLIB_CPP_LANG_VERSION >= KLIB_CPP23
+    #define KLIB_HAS_CPP23 1
+#else
+    #define KLIB_HAS_CPP23 0
+#endif
+
+#ifndef KLIB_ENV_WINDOWS
+    #define KLIB_ENV_WINDOWS 0
+#endif
+
+#ifndef KLIB_ENV_UNIX
+    #define KLIB_ENV_UNIX 0
+#endif
+
+#ifndef KLIB_COMPILER_MSVC
+    #define KLIB_COMPILER_MSVC 0
+#endif
+
+#ifndef KLIB_COMPILER_MINGW
+    #define KLIB_COMPILER_MINGW 0
+#endif
+
+#ifndef KLIB_ENV_APPLE
+    #define KLIB_ENV_APPLE 0
+#endif
+
+#ifndef KLIB_ENV_LINUX
+    #define KLIB_ENV_LINUX 0
+#endif
+
+#ifndef KLIB_ENV_ARM64
+    #define KLIB_ENV_ARM64 0
+#endif
+
+#ifndef KLIB_ENV_X64
+    #define KLIB_ENV_X64 0
+#endif
+
+// インスタンス化できないようにする
+#define KLIB_STATIC_CLASS(className) \
+    className() = delete; \
+    className(className const&) = delete; \
+    className(className&&) = delete; \
+    ~className() = delete; \
+    className& operator=(className const&) = delete; \
+    className& operator=(className&&) = delete
+
+/// <summary>
+/// 既定の名前空間
+/// </summary>
+namespace klib
+{
+
+}
+
+/// <summary>
+/// 基本クラスたち
+/// </summary>
+namespace klib::Kongkong
+{
+    template <class T>
+    concept CCharType =
+        ::std::same_as<T, char> ||
+        ::std::same_as<T, wchar_t> ||
+        ::std::same_as<T, char8_t> ||
+        ::std::same_as<T, char16_t> ||
+        ::std::same_as<T, char32_t>;
+
+    class GC;
+    class Object;
+
+    template <CCharType TChar>
+    struct GenericString;
+}
+
+/// <summary>
+/// コマンドライン
+/// </summary>
+namespace klib::Kongkong::Cli
+{
+    class CliOption;
+}
+
+/// <summary>
+/// コンテナ型
+/// </summary>
+namespace klib::Kongkong::Containers
+{
+
+}
+
+namespace klib::Kongkong::IO
+{
+
+}
+
+/// <summary>
+/// 数値関連
+/// </summary>
+namespace klib::Kongkong::Numerics
+{
+
+}
+
+namespace klib::Kongkong::Text
+{
+    template <CCharType T>
+    class GenericStringBase;
+}
+
+namespace klib::Kongkong::Threading
+{
+
+}
+
+// エイリアス
+namespace klib::Kongkong
+{
+    using String = GenericString<char16_t>;
+    using CharString = GenericString<char>;
+    using WCharString = GenericString<wchar_t>;
+    using Utf8String = GenericString<char8_t>;
+    using Utf16String = GenericString<char16_t>;
+    using Utf32String = GenericString<char32_t>;
+}
+
+namespace klib::Kongkong::Text
+{
+    using StringBase = GenericStringBase<char16_t>;
+    using CharStringBase = GenericStringBase<char>;
+    using WCharStringBase = GenericStringBase<wchar_t>;
+    using Utf8StringBase = GenericStringBase<char8_t>;
+    using Utf16StringBase = GenericStringBase<char16_t>;
+    using Utf32StringBase = GenericStringBase<char32_t>;
+}
+
+#endif //!KLIB_BASE_H
---
+++ src/cpp/All.h
@@ -0,0 +1,10 @@
+﻿#ifndef ALL_H
+#define ALL_H
+
+#include <klib/impl/base.h>
+
+#if KLIB_COMPILER_MSVC
+    #pragma warning(disable:4996)
+#endif
+
+#endif //!ALL_H
